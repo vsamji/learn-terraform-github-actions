d5561f416e7c25cc5bef5809dcebfa01d49194ac
vsamji <vsamji@splunk.com>
2021-07-07 16:07:46 +0100

    Point backend to correct TFC org and workspace 3

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ provider "aws" {
 resource "random_pet" "sg" {}
 
 resource "aws_instance" "web" {
-  ami                    = "ami-830c94e3"
+  ami                    = "ami-0ab4d1e9cf9a1215a"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
